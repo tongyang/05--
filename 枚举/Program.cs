f1e4cf0e3597cfbdfe6c3d80237bd75bedf8de55
扬 童 <452949529@qq.com>
2018-06-23 00:09:17 +0800

    6.23 0:09

---
+++ 枚举/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 枚举
+{
+    //public enum Seasons
+    //{
+    //    春,
+    //    夏,
+    //    秋,
+    //    冬
+    //}
+
+    enum State
+    {
+        在线 = 1,
+        离线,
+        离开,
+        忙碌,
+        Q我
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String s = "asdas";
+            //try
+            //{
+            //    Seasons season = (Seasons)Enum.Parse(typeof(Seasons), s);
+            //    Console.WriteLine(season);
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+            Console.WriteLine("请选择您的QQ在线状态：1--在线 2--离线 3--离开 4--忙碌 5--Q我");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    print(input);
+                    break;
+
+                case "2":
+                    print(input);
+                    break;
+                case "3":
+                    print(input);
+                    break;
+                case "4":
+                    print(input);
+                    break;
+                case "5":
+                    print(input);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+
+        static void print(string input)
+        {
+            try
+            {
+                State state = (State)Enum.Parse(typeof(State), input);
+                Console.WriteLine("您选择的在线状态为：{0}", state);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ 结构/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 结构
+{
+    struct MyColor
+    {
+        public int _red;
+        public int _green;
+        public int _blue;
+    }
+
+    struct Person
+    {
+        public string _name;
+        public Gender _gender;
+        public int _age;
+    }
+
+    enum Gender
+    {
+        男,
+        女
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyColor myColor;
+            myColor._blue = 0;
+            myColor._green = 0;
+            myColor._red = 255;
+            //showColor(myColor);
+
+            Person zhangsan;
+            zhangsan._name = "张三";
+            zhangsan._gender = Gender.男;
+            zhangsan._age = 18;
+            Console.WriteLine("{0} {1} {2}岁", zhangsan._name, zhangsan._gender, zhangsan._age);
+
+
+            Person xiaolan;
+            xiaolan._name = "小兰";
+            xiaolan._gender = Gender.女;
+            xiaolan._age = 16;
+            Console.WriteLine("{0} {1} {2}岁", xiaolan._name, xiaolan._gender, xiaolan._age);
+        }
+
+        static void showColor(MyColor myColor)
+        {
+            if (myColor._red == 255 && myColor._green == 0 && myColor._blue == 0)
+            {
+                Console.WriteLine("当前颜色为红色");
+            }
+        }
+    }
+}
