e8cd511aaac79910bcebd99450771f802daec0dd
扬 童 <452949529@qq.com>
2018-06-23 11:41:04 +0800

    完成第一个练习

--- 数组/Program.cs
+++ 数组/Program.cs
@@ -10,6 +10,27 @@ namespace 数组
     {
         static void Main(string[] args)
         {
+            int[] nums = {1, 2, 3, 4, 5, 0, 6, 2, 55};
+            int max = nums[0];
+            int min = nums[0];
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (max < nums[i])
+                {
+                    max = nums[i];
+                }
+                if (min > nums[i])
+                {
+                    min = nums[i];
+                }
+                sum += nums[i];
+            }
+
+            int avg = sum / nums.Length;
+
+            Console.WriteLine("最小值：{0}  最大值：{1}  总和：{2}  平局值：{3}", min, max, sum, avg);
         }
     }
 }
