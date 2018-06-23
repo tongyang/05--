f39af759c52e999583aa0ed11550036ad74dbf3c
扬 童 <452949529@qq.com>
2018-06-23 13:08:10 +0800

    第二个练习

--- 数组/Program.cs
+++ 数组/Program.cs
@@ -10,6 +10,7 @@ namespace 数组
     {
         static void Main(string[] args)
         {
+            #region 第一个练习
             int[] nums = {1, 2, 3, 4, 5, 0, 6, 2, 55};
             int max = nums[0];
             int min = nums[0];
@@ -31,6 +32,26 @@ namespace 数组
             int avg = sum / nums.Length;
 
             Console.WriteLine("最小值：{0}  最大值：{1}  总和：{2}  平局值：{3}", min, max, sum, avg);
+            #endregion
+
+            #region 第二个练习
+
+            string[] strs = {"我", "是", "好人"};
+            // 注意不要循环那么多次，循环数组长度/2就可以了，循环Length，会又交换回来
+            for (int i = 0; i < strs.Length / 2; i++)
+            {
+                string temp = strs[i];
+                strs[i] = strs[strs.Length - 1 - i];
+                strs[strs.Length - 1 - i] = temp;
+            }
+
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                Console.Write(strs[i]);
+            }
+
+            #endregion
         }
     }
 }
