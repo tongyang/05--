b5d777992e7f81bec5a43e072ed794c16fea081e
扬 童 <452949529@qq.com>
2018-06-23 14:00:36 +0800

    完成冒泡排序

---
+++ 冒泡排序/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 冒泡排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 4, 5, 6, 11, 7, 32, 22, 31 };
+            //for (int i = 0; i < numbers.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < numbers.Length - 1 - i; j++)
+            //    {
+            //        if (numbers[j] > numbers[j + 1])
+            //        {
+            //            int temp = numbers[j];
+            //            numbers[j] = numbers[j + 1];
+            //            numbers[j + 1] = temp;
+            //        }
+            //    }
+            //}
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+                
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
